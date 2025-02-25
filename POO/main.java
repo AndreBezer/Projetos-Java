8f3c8fea5d428d6a7f00e3ea30e0d36fab0db9d6
AndreLjesus <143151297+AndreLjesus@users.noreply.github.com>
2025-02-25 08:58:01 -0300

    Minha primeira Class com Java

---
+++ POO/main.java
@@ -0,0 +1,21 @@
+package ProjetosJava.POO;
+
+// Class main
+public class main {
+    public static void main(String[] args) {
+        
+        // Criação do objeto Loading
+        Loading loading = new Loading();
+        loading.Loading();
+
+    }
+}
+
+// Classe Loading com o metodo LoadingMessage
+class Loading{
+
+    // Metodo
+    void Loading(){
+        System.out.println("Loading");
+    }
+}
